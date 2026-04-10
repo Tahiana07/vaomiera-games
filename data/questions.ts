bd6dc90a9a65ecd2dfdc8dedb3c74a80b8a6b7c5
Tahiana07 <rasolofomanananjakatahiana@gmail.com>
2026-04-10 20:29:37 +0100

    update questions

--- data/questions.ts
+++ data/questions.ts
@@ -35,4 +35,67 @@ export const questions = [
     ],
     answer: "Mikolo ny ara-batana"
   },
+  {
+    question: "Iza no Vaomiera niforona aingana indrindra?",
+    options: [
+      "Vaomieran'ny fitaovana",
+      "Vaomieran'ny ankizy kely",
+      "Vaomieran'ny fitaizana ara-batana"
+    ],
+    answer: "Vaomieran'ny fitaovana"
+  },
+  {
+    question: "Iza no mizara partition rehefa mianatra hira?",
+    options: [
+      "Vaomieran'ny fitaovana",
+      "Vaomieran'ny zava-kanto",
+      "Vaomieran'ny ara-panahy"
+    ],
+    answer: "Vaomieran'ny zava-kanto"
+  },
+  {
+    question: "Firy no isan'ny Vaomiera?",
+    options: [
+      "6",
+      "7",
+      "8"
+    ],
+    answer: "7"
+  },
+  {
+    question: "Iza no Vaomiera manomana lasy?",
+    options: [
+      "Vaomieran'ny ara-panahy",
+      "Vaomieran'ny ankizy kely",
+      "Vaomieran'ny ara-batana"
+    ],
+    answer: "Vaomieran'ny ara-panahy"
+  },
+  {
+    question: "Iza no miandraikitra micro, sonorisation sy fitaovana hafa rehefa Krismasy?",
+    options: [
+      "Vaomieran'ny fitaovana",
+      "Vaomieran'ny zava-kanto",
+      "Vaomieran'ny ara-panahy"
+    ],
+    answer: "Vaomieran'ny fitaovana"
+  },
+  {
+    question: "Iza no mikarakara randonnée culturelle sy reboisement?",
+    options: [
+      "Vaomieran'ny hetsika sy serasera",
+      "Vaomieran'ny ara-panahy",
+      "Vaomieran'ny kolontsaina"
+    ],
+    answer: "Vaomieran'ny kolontsaina"
+  },
+  {
+    question: "Iza no manao publication amin'ny page Facebook an'ny STKRZaAF?",
+    options: [
+      "Vaomieran'ny hetsika sy serasera",
+      "Vaomieran'ny fitaizana ara-batana",
+      "Vaomieran'ny zava-kanto"
+    ],
+    answer: "Vaomieran'ny hetsika sy serasera"
+  },
 ];
