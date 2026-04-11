762f248ca95ebd5e890f2e54faf6e8bb6cf6cb0b
Tahiana07 <rasolofomanananjakatahiana@gmail.com>
2026-04-11 13:18:06 +0100

    last debug and fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export function getSupabase() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const supabaseUrl = "https://lfzqefuhybazyfwauxqu.supabase.co";
+  const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmenFlZnVoeWJhenlmd2F1eHF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4NjY4MTksImV4cCI6MjA5MTQ0MjgxOX0.sjmzi3xrERQGKSPCfPDd21FvHo1YNmw6YR9nJ3AjMFI";
 
   if (!supabaseUrl || !supabaseKey) {
     throw new Error("Supabase env variables missing");
